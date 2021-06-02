8c4541316ddf05822b79ae13fd8f16751dc99877
Sebastian <sebastian.adorsch@gmail.com>
2021-06-02 13:02:50 -0300

    Vista home con posteos

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    #path('', views.home, name="home"),
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
 ]
