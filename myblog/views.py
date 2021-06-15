0672fef6a88efbdedb1a195aa62ec066ca8a082f
sebastian <sdorsch@lacapital.com.ar>
2021-06-15 11:06:46 -0300

    Verificacion de usuario para editar y borrar posts

--- myblog/views.py
+++ myblog/views.py
@@ -10,6 +10,12 @@ class HomeView(ListView):
     template_name = 'home.html'
     ordering = ['-post_datetime']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats.lower().replace('-', ' '))
