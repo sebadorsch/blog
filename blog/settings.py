32b0dc129bb547b73b03a6d20bb27566b90abac5
sebastian <sdorsch@lacapital.com.ar>
2021-07-05 13:38:04 -0300

    Cargando el perfil pero los usuarios sin imagen no anda

--- blog/settings.py
+++ blog/settings.py
@@ -125,11 +125,17 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
-)
+        )
 
 
 # Default primary key field type
--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,4 +9,5 @@ urlpatterns = [
     #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
     path('password_succes', views.password_succes, name='password_succes'),
+    path('<int:pk>/profile', ShowProfilePageView.as_view(), name='show_profile_page')
 ]
