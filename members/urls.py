aec7eed11d7b15c6c9a8ab03b0ce1c4e779e85bf
sebastian <sdorsch@lacapital.com.ar>
2021-07-06 14:02:25 -0300

    Carga perfil andando

--- members/urls.py
+++ members/urls.py
@@ -6,8 +6,7 @@ from . import views
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
     path('edit_profile/', UserEditView.as_view(), name="edit_profile"),
-    #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
     path('password_succes', views.password_succes, name='password_succes'),
-    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name="show_profile_page"),
 ]
