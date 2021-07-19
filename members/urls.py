32b8b7aed9306f684ad8011f955da82eb07dadf7
sebastian <sdorsch@lacapital.com.ar>
2021-07-19 14:02:29 -0300

    Carga perfil andando

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, CreateProfilePageView, EditProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
     path('password_succes', views.password_succes, name='password_succes'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name="show_profile_page"),
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name="edit_profile_page"),
+    path('create_profile_page/', CreateProfilePageView.as_view(), name="create_profile_page"),
 ]
--- members/views.py
+++ members/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm
 from myblog.models import Profile
+
+
+class CreateProfilePageView(CreateView):
+    model = Profile
+    form_class = ProfilePageForm
+    template_name = "registration/create_user_profile_page.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'linkedin_url']
+
+    success_url = reverse_lazy('home')
 
 
 class ShowProfilePageView(DetailView):
