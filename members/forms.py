93afbf50490e4bcafae2e087668828796416ddc7
sebastian <sdorsch@lacapital.com.ar>
2021-07-21 13:42:11 -0300

    Aplicando algunos estilos

--- members/forms.py
+++ members/forms.py
@@ -48,10 +48,24 @@ class EditProfileForm(UserChangeForm):
         fields = ('username', 'first_name', 'last_name', 'email', 'password')
 
 
+class EditProfileFormPage(UserChangeForm):
+    website_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    facebook_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    twitter_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    instagram_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    linkedin_url = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Profile
+        fields = ('bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'linkedin_url')
+
+
 class PasswordChangingForm(PasswordChangeForm):
     old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}))
-    new_password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}))
-    new_password2 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}))
+    new_password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'type': 'password'}))
+    new_password2 = forms.CharField(max_length=100, widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'type': 'password'}))
 
     class Meta:
         model = User
--- members/views.py
+++ members/views.py
@@ -4,7 +4,7 @@ from django.views.generic import DetailView, CreateView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm, EditProfileFormPage
 from myblog.models import Profile
 
 
@@ -19,11 +19,21 @@ class CreateProfilePageView(CreateView):
 
 
 class EditProfilePageView(generic.UpdateView):
+    form_class = EditProfileFormPage
     model = Profile
     template_name = 'registration/edit_profile_page.html'
-    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'linkedin_url']
+    #fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'linkedin_url']
 
     success_url = reverse_lazy('home')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
 
 
 class ShowProfilePageView(DetailView):
@@ -55,10 +65,3 @@ class UserRegisterView(generic.CreateView):
     success_url = reverse_lazy('login')
 
 
-class UserEditView(generic.UpdateView):
-    form_class = EditProfileForm
-    template_name = 'registration/edit_profile.html'
-    success_url = reverse_lazy('home')
-
-    def get_object(self):
-        return self.request.user
